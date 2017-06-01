854869785530baf53ba91b7d2353ac6daa4556d9
stefthor <stefthor951@ed.amdsb.ca>
2017-06-01 14:53:02 -0400

    Add project files.

---
+++ RunningGame/Screens/MenuScreen.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RunningGame.Screens
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.highscoreLabel = new System.Windows.Forms.Label();
+            this.exitLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.Color.White;
+            this.startLabel.Location = new System.Drawing.Point(70, 323);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(126, 40);
+            this.startLabel.TabIndex = 0;
+            this.startLabel.Text = "START";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.ForeColor = System.Drawing.Color.White;
+            this.instructionLabel.Location = new System.Drawing.Point(257, 323);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(209, 40);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "CONTROLS";
+            // 
+            // highscoreLabel
+            // 
+            this.highscoreLabel.AutoSize = true;
+            this.highscoreLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highscoreLabel.ForeColor = System.Drawing.Color.White;
+            this.highscoreLabel.Location = new System.Drawing.Point(527, 323);
+            this.highscoreLabel.Name = "highscoreLabel";
+            this.highscoreLabel.Size = new System.Drawing.Size(249, 40);
+            this.highscoreLabel.TabIndex = 2;
+            this.highscoreLabel.Text = "HIGHSCORES";
+            // 
+            // exitLabel
+            // 
+            this.exitLabel.AutoSize = true;
+            this.exitLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitLabel.ForeColor = System.Drawing.Color.White;
+            this.exitLabel.Location = new System.Drawing.Point(837, 323);
+            this.exitLabel.Name = "exitLabel";
+            this.exitLabel.Size = new System.Drawing.Size(93, 40);
+            this.exitLabel.TabIndex = 3;
+            this.exitLabel.Text = "EXIT";
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.Controls.Add(this.exitLabel);
+            this.Controls.Add(this.highscoreLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.startLabel);
+            this.Name = "MenuScreen";
+            this.Size = new System.Drawing.Size(1000, 400);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MenuScreen_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.MenuScreen_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label highscoreLabel;
+        private System.Windows.Forms.Label exitLabel;
+    }
+}
---
+++ RunningGame/Screens/MenuScreen.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningGame.Screens
+{
+    public partial class MenuScreen : UserControl
+    {
+        int index = 0;
+        int lastIndex = 0;
+        bool leftKeyDown, rightKeyDown;
+
+        public MenuScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftKeyDown = false;
+                    break;
+                case Keys.Right:
+                    rightKeyDown = false;
+                    break;
+            }
+        }
+
+        private void MenuScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            lastIndex = index;
+            Form form = this.FindForm();
+            Graphics g = this.CreateGraphics();
+            Pen whitePen = new Pen(Color.White, 5);
+            //to make sure that if you click to the bottom or top you will go to the other end instead of just stopping
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (leftKeyDown == false)
+                    {
+                        leftKeyDown = true;
+                        if (index != 0)
+                            index--;
+                        else
+                        {
+                            index = 3;
+                        }
+                    }
+                    break;
+                case Keys.Right:
+                    if (rightKeyDown == false)
+                    {
+                        rightKeyDown = true;
+                        if (index != 3)
+                            index++;
+                        else
+                        {
+                            index = 0;
+                        }
+                    }
+                    break;
+
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+
+                //Selecting an option on the main menu
+                case Keys.Space:
+                    switch (index)
+                    {
+                        //start button
+                        case 0:
+
+                            GameScreen gs = new GameScreen();
+                            gs.Location = new Point((form.Width - gs.Width) / 2, (form.Height - gs.Height) / 2);
+
+                            form.Controls.Add(gs);
+                            form.Controls.Remove(this);
+
+                            break;
+
+                        //highscore button
+                        case 1:
+
+                            InstructionScreen ins = new InstructionScreen();
+
+                            form.Controls.Add(ins);
+                            form.Controls.Remove(this);
+
+                            ins.Location = new Point((form.Width - ins.Width) / 2, (form.Height - ins.Height) / 2);
+                            break;
+                            
+
+                        //option button
+                        case 2:
+
+                            HighscoreScreen hs = new HighscoreScreen();
+                            form.Controls.Add(hs);
+                            form.Controls.Remove(this);
+
+                            hs.Location = new Point((form.Width - hs.Width) / 2, (form.Height - hs.Height) / 2);
+
+                            break;
+
+                        //exit button
+                        case 3:
+
+                            Application.Exit();
+                            break;
+                    }
+                    break;
+            }
+
+            //set button to white if not clicked on
+            switch (lastIndex)
+            {
+                case 0:
+                    startLabel.ForeColor = Color.White;
+                    break;
+                case 1:
+                    instructionLabel.ForeColor = Color.White;
+                    break;
+                case 2:
+                    highscoreLabel.ForeColor = Color.White;
+                    break;
+                case 3:
+                    exitLabel.ForeColor = Color.White;
+                    break;
+            }
+
+            //set selected button to red
+            switch (index)
+            {
+                case 0:
+                    g.Clear(Color.Transparent);
+                    g.DrawLine(whitePen, startLabel.Location.X, startLabel.Location.Y + startLabel.Size.Height, startLabel.Location.X + startLabel.Size.Width, startLabel.Location.Y + startLabel.Size.Height);
+                    break;
+                case 1:
+                    g.Clear(Color.Transparent);
+                    g.DrawLine(whitePen, instructionLabel.Location.X, instructionLabel.Location.Y + instructionLabel.Size.Height, instructionLabel.Location.X + instructionLabel.Size.Width, instructionLabel.Location.Y + instructionLabel.Size.Height);
+                    break;
+                case 2:
+                    g.Clear(Color.Transparent);
+                    g.DrawLine(whitePen, highscoreLabel.Location.X, highscoreLabel.Location.Y + highscoreLabel.Size.Height, highscoreLabel.Location.X + highscoreLabel.Size.Width, highscoreLabel.Location.Y + highscoreLabel.Size.Height);
+                    break;
+                case 3:
+                    g.Clear(Color.Transparent);
+                    g.DrawLine(whitePen, exitLabel.Location.X, exitLabel.Location.Y + exitLabel.Size.Height, exitLabel.Location.X + exitLabel.Size.Width, exitLabel.Location.Y + exitLabel.Size.Height);
+                    break;
+            }
+        }
+    }
+}
